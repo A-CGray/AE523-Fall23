f933d818d49c7e359476117b27f8e8b38a13b229
Alasdair Gray <alachris@umich.edu>
2023-09-11 11:41:56 -0400

    add grad descent

--- Examples/Week3/FiniteDifferenceScheme.py
+++ Examples/Week3/FiniteDifferenceScheme.py
@@ -5,7 +5,7 @@
 #
 # These examples are based on code originally written by Krzysztof Fidkowski and adapted by Venkat Viswanathan.
 
-# In[134]:
+# In[2]:
 
 
 import jax
@@ -33,7 +33,7 @@ plt.style.use(niceplots.get_style())
 # where $\kappa$ is the thermal conductivity, $T$ is the temperature, $x$ is the position, and $q(x)$ is a heat source term, which in this case is $\sin\left(\pi x /L\right)$.
 #
 
-# In[135]:
+# In[3]:
 
 
 # Define the symbolic function q(x)
@@ -95,7 +95,7 @@ def q(x, L):
 #
 # The code below implements the second approach, and solves the matrix equation using a direct sparse linear solver.
 
-# In[136]:
+# In[4]:
 
 
 def heat_conduction(T_left=1.0, T_right=4.0, L=2.0, kappa=0.5, Nx=10):
@@ -158,7 +158,7 @@ def heat_conduction(T_left=1.0, T_right=4.0, L=2.0, kappa=0.5, Nx=10):
 #
 # $$T_\text{exact} (x) = \frac{L^2}{\kappa \pi^2}\sin\left(\frac{\pi x}{L}\right) + T_0 + \frac{x}{L}\left(T_N - T_0\right)$$
 
-# In[137]:
+# In[5]:
 
 
 # Define the true solution
@@ -202,7 +202,7 @@ plt.show()
 #
 # This is not equivalent to the typical $L_2$ computed by numpy/JAX, which do not normalize by the number of elements, and would therefore give the wrong order of convergence.
 
-# In[138]:
+# In[6]:
 
 
 Nsweep = 2 ** jnp.arange(1, 8)
@@ -235,7 +235,7 @@ niceplots.adjust_spines(ax)
 #
 # In this case, we will generate our "experimental data" by running the FD code with a given value of $\kappa$, this way we can tell if our learning process has been successful.
 
-# In[139]:
+# In[7]:
 
 
 Nx = 10
@@ -244,7 +244,7 @@ measured_temps, measurement_locations = heat_conduction(T0, TN, L, 0.5, Nx)
 
 # Next we define our "loss" or "objective" function, this is the function that we will try to minimize. In this case, we will use the $L_2$ norm of the difference between the measured temperatures and the temperatures computed by the FD code. We will also use JAX to create a function that computes $\frac{df}{d\kappa}$.
 
-# In[140]:
+# In[8]:
 
 
 def obj_function(kappa):
@@ -263,10 +263,10 @@ obj_grad = grad(obj_function)
 
 # Now we will use an algorithm called an optimizer to find the value of $\kappa$ that minimizes the objective function. For problems where we expect the objective function to be smooth, gradient-based optimizers are the most efficient methods. Here we use an optimizer provided by JAX which will use the gradient information computed by JAX's AD to find the minimum.
 
-# In[142]:
+# In[9]:
 
 
-sol = minimize(obj_function, 2.349822365, jac=obj_grad, method="SLSQP", tol=1e-6)
+sol = minimize(obj_function, 2.349822365, jac=obj_grad, method="SLSQP", tol=1e-6, options={"disp": True})
 
 if sol.success:
     print(f"\nSUCCESS: My best guess at kappa is {sol.x[0]:.7e}")
@@ -274,4 +274,31 @@ else:
     print("\nFAILURE: Optimization did not converge")
 
 
-# In[ ]:
+# Another optimization algorithm we could use is gradient-descent. There are a few different versions of gradient descent, but here we will use the simplest one. At each iteration we compute the gradient of the objective function and then simply take a step in the downhill direction, scaled by a value $\lambda$ which people in the machine learning community like to call the "learning rate":
+#
+# $$x_{i+1} = x_i - \lambda \frac{df}{dx}$$
+
+# In[27]:
+
+
+def gradient_descent(f, grad_f, x0, step_size=0.01, max_iter=1000, tol=1e-6):
+    x = x0
+    converged = False
+    for ii in range(max_iter):
+        func_val = f(x)
+        dfdx = grad_f(x)
+        if jnp.linalg.norm(dfdx) < tol:
+            converged = True
+            break
+        x = x - step_size * dfdx
+    if converged:
+        print(f"Converged after {ii} iterations")
+    else:
+        print(f"Did not converge after {max_iter} iterations")
+    return x, func_val
+
+
+kappa_grad_descent, error_grad_descent = gradient_descent(obj_function, obj_grad, jnp.array([2.349822365]))
+
+
+# As you can see, gradient descent is not a very good algorithm, even with 1000 function evaluations, it fails to get as close to the true answer as SLSQP did in only 26 evaluations. This performance can be improved by tuning the learning rate, but this process is very tedious and requires a lot of trial and error.
